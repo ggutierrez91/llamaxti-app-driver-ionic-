b3814a3dd0769eea6768b538ae5bb2b9c1e48912
william <william.usp@hotmail.com>
2020-11-25 17:51:38 -0500

    appDriver avance mie 25 nov

--- src/app/pipes/payment.pipe.ts
+++ src/app/pipes/payment.pipe.ts
@@ -3,6 +3,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'payment'})
 export class PaymentPipe implements PipeTransform {
     transform(code: string): any {
-        return code === 'CASH' ? 'Efectivo' : 'Tarjeta';
+        let str = 'Efectivo';
+        switch (code) {
+            case 'CASH':
+                str = 'Efectivo';
+                break;
+                case 'CARD':
+                    str = 'Tarjeta';
+                    break;
+                    case 'CRED':
+                        str = 'Llamacréditos';
+                        break;
+            default:
+                break;
+        }
+        return str;
     }
 }
